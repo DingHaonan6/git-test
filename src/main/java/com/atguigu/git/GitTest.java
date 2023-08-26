5ddbad5f9308e338a578a62749e027c694cc21d1
DingHaonan <583985093@qq.com>
2023-08-26 12:55:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author Ding Haonan
+ * @version 1.0
+ * @create 2023-08-26-12:51
+ * @description description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello ,my git!!");
+    }
+}
