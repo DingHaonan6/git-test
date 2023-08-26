79f24c5dc438f7b41c73e3caf9495811c6546d54
DingHaonan <583985093@qq.com>
2023-08-26 13:07:04 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,8 +8,8 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello ,my git!!");
         System.out.println("hello ,my git2!!");
         System.out.println("hello ,my git3!!");
+        System.out.println("hello ,my git4!!");
     }
 }
