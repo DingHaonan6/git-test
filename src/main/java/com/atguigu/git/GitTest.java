a34cf9fb48a5c40adf6435405da463641b98e10e
DingHaonan <583985093@qq.com>
2023-08-26 12:59:18 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello ,my git!!");
         System.out.println("hello ,my git2!!");
+        System.out.println("hello ,my git3!!");
     }
 }
